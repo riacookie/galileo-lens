270d9fdec02bf726fe22cb1ffec163b5527a7746
Michael-Mai <91682383+Michael-Mai@users.noreply.github.com>
2025-06-15 10:51:39 +0700

    fixed  navigation

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Optional, if you're using React Router
+import Link from 'next/link';
 
 const Nav: React.FC = () => {
   return (
     <nav className="bg-gray-800 text-white p-4 flex justify-between">
       <div className="font-bold text-xl">Galileo lens</div>
       <ul className="flex space-x-4">
-        <li><Link to="../earth">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link href="../earth">earth</Link></li>
+        <li><Link href="../luna">luna</Link></li>
+        <li><Link href="../satellite">satellite</Link></li>
       </ul>
     </nav>
   );

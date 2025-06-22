3ed8af76c66bb886f21bc94466beb3c4c61345ef
Michael-Mai <91682383+Michael-Mai@users.noreply.github.com>
2025-06-22 11:56:40 +0700

    Changes: - added navbar transparency and functionality - added scroll effect (stll in development)

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,22 +2,34 @@
 import React from "react";
 import Link from "next/link";
 const Nav: React.FC = () => {
-  
 
+	let LastScroll = window.scrollY;
+	const navbar = document.getElementById("navbar");
 
+	window.addEventListener("scroll", () => {
+		if (navbar) {
+			if (window.scrollY > LastScroll) {
+				navbar.classList.add("hidden");
+			} else {
+				navbar.classList.remove("hidden");
+			}
+			LastScroll = window.scrollY;
+		}
+	});
 
 	return (
-		<nav className="bg-gray-800 text-white p-4 flex justify-between">
-			<div className="font-bold text-xl">Galileo lens</div>
+	<header className="fixed top-0 left-0 right-0 border-b border-none bg-white-900/50 backdrop-blur-sm">
+	<nav id="navbar" className="p-5 flex justify-between">
+		<Link className="font-bold text-4xl" href="../">Galileo lens</Link>
 
       <div className="flex items-center space--4">
-			<ul className="flex space-x-4 hover text-slate-300">
+			<ul className="flex space-x-6 hover text-slate-300 text-2xl">
 				<li className="nav-underline-animate">
 					<Link 
 					className="duration-300 ease-in-out inline-block nav-text-hover"
 					href="../earth">
 						Earth
-					</Link>
+					</Link>		
 				</li>
 				<li className="nav-underline-animate">
 					<Link 
@@ -36,7 +48,8 @@ const Nav: React.FC = () => {
 			</ul>
       </div>
 
-		</nav>
+	</nav>
+	</header>
 	);
 };
 

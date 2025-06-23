75a3eabed32f8651f62e465feec6880ee29e07b9
oofie <tdkmike7406@gmail.com>
2025-06-23 19:56:12 +0700

    Refactor content in Home component for improved readability and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,44 @@ export default function Home() {
 							Discover intriguing insights about our universe, spanning from
 							Earth to the Moon and satellites. Galileo Lens leverages
 							NASA&apos;s observation APIs to deliver the latest data, including
-							stunning satellite imagery.
-
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque, est ac gravida efficitur, risus erat consequat arcu, et rhoncus dolor ligula eu nisl. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam eleifend tortor sit amet nisi porttitor molestie. Vivamus orci justo, luctus sit amet ligula at, molestie lacinia tortor. Morbi et nibh risus. Pellentesque sagittis non lacus at dictum. Suspendisse eu orci molestie, ultricies nisl luctus, gravida enim. Maecenas sit amet tortor mauris. Duis in nunc porttitor, fermentum massa non, porttitor turpis.
-
-              Donec ut libero sed libero vehicula sollicitudin a at metus. Nam non dolor eget magna molestie eleifend. Aenean orci metus, fermentum a neque ac, rutrum dapibus erat. Praesent gravida dui a nisi sagittis, eu suscipit est tincidunt. Morbi sem nisi, consectetur a sagittis eget, auctor sit amet diam. Nulla sodales pellentesque pharetra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-              Maecenas id mollis mauris. Curabitur mattis velit id tortor scelerisque, quis vulputate magna blandit. Nulla et quam a est tincidunt ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Praesent tempor nulla felis, in ornare arcu vehicula in. Donec a tincidunt massa. Donec vitae molestie leo, vitae ullamcorper velit. Donec mollis vel elit id dignissim. Fusce ut cursus risus.
-
-              Donec efficitur nunc sit amet posuere ultrices. Mauris sodales laoreet ornare. Sed vitae ante quis magna ultrices tristique. Phasellus lobortis vulputate lorem eget venenatis. In sed tortor a felis pretium cursus eget vel est. Vivamus eget imperdiet erat. Integer ultrices, nulla nec luctus luctus, elit risus pretium urna, sed aliquam ex felis at mauris. In eget erat at neque feugiat dignissim at nec velit.
-
-              Quisque fermentum arcu nec fermentum mollis. Nam volutpat interdum purus sit amet venenatis. Morbi aliquet lectus mi, ac mattis ligula porttitor vel. Proin sit amet imperdiet massa. Ut molestie luctus dignissim. Suspendisse id enim et tortor viverra dapibus at id augue. Fusce auctor nulla sed ligula egestas malesuada. Aenean tempor lacus quis enim elementum mollis. Curabitur gravida ex egestas neque efficitur convallis. Aliquam laoreet mollis lobortis. Praesent semper at lectus a venenatis.
+							stunning satellite imagery. Lorem ipsum dolor sit amet,
+							consectetur adipiscing elit. Integer pellentesque, est ac gravida
+							efficitur, risus erat consequat arcu, et rhoncus dolor ligula eu
+							nisl. Class aptent taciti sociosqu ad litora torquent per conubia
+							nostra, per inceptos himenaeos. Nam eleifend tortor sit amet nisi
+							porttitor molestie. Vivamus orci justo, luctus sit amet ligula at,
+							molestie lacinia tortor. Morbi et nibh risus. Pellentesque
+							sagittis non lacus at dictum. Suspendisse eu orci molestie,
+							ultricies nisl luctus, gravida enim. Maecenas sit amet tortor
+							mauris. Duis in nunc porttitor, fermentum massa non, porttitor
+							turpis. Donec ut libero sed libero vehicula sollicitudin a at
+							metus. Nam non dolor eget magna molestie eleifend. Aenean orci
+							metus, fermentum a neque ac, rutrum dapibus erat. Praesent gravida
+							dui a nisi sagittis, eu suscipit est tincidunt. Morbi sem nisi,
+							consectetur a sagittis eget, auctor sit amet diam. Nulla sodales
+							pellentesque pharetra. Orci varius natoque penatibus et magnis dis
+							parturient montes, nascetur ridiculus mus. Maecenas id mollis
+							mauris. Curabitur mattis velit id tortor scelerisque, quis
+							vulputate magna blandit. Nulla et quam a est tincidunt ultrices.
+							Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+							posuere cubilia curae; Praesent tempor nulla felis, in ornare arcu
+							vehicula in. Donec a tincidunt massa. Donec vitae molestie leo,
+							vitae ullamcorper velit. Donec mollis vel elit id dignissim. Fusce
+							ut cursus risus. Donec efficitur nunc sit amet posuere ultrices.
+							Mauris sodales laoreet ornare. Sed vitae ante quis magna ultrices
+							tristique. Phasellus lobortis vulputate lorem eget venenatis. In
+							sed tortor a felis pretium cursus eget vel est. Vivamus eget
+							imperdiet erat. Integer ultrices, nulla nec luctus luctus, elit
+							risus pretium urna, sed aliquam ex felis at mauris. In eget erat
+							at neque feugiat dignissim at nec velit. Quisque fermentum arcu
+							nec fermentum mollis. Nam volutpat interdum purus sit amet
+							venenatis. Morbi aliquet lectus mi, ac mattis ligula porttitor
+							vel. Proin sit amet imperdiet massa. Ut molestie luctus dignissim.
+							Suspendisse id enim et tortor viverra dapibus at id augue. Fusce
+							auctor nulla sed ligula egestas malesuada. Aenean tempor lacus
+							quis enim elementum mollis. Curabitur gravida ex egestas neque
+							efficitur convallis. Aliquam laoreet mollis lobortis. Praesent
+							semper at lectus a venenatis.
 						</p>
 					</div>
 				</div>
